ca34a2089ff705a6532b824093855cf797378737
filip <fbielejec@gmail.com>
2019-06-21 14:14:33 +0200

    add dep

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject district0x/async-helpers "0.1.0-SNAPSHOT"
+(defproject district0x/async-helpers "0.1.1-SNAPSHOT"
   :description "library of helper functions"
   :url "https://github.com/district0x/async-helpers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [com.taoensso/timbre "4.10.0"]]
+  :dependencies [[com.taoensso/timbre "4.10.0"]
+                 [district0x/error-handling "1.0.4"]
+                 [org.clojure/clojurescript "1.10.339"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
