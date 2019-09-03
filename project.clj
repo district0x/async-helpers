d272179c0b7facaa5693d9a1372f237a0628ebc1
Juan Monetta <jpmonettas@gmail.com>
2019-09-03 08:29:11 -0300

    Add safe-go and <?, extend-promises-as-channels!

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject district0x/async-helpers "0.1.1-SNAPSHOT"
+(defproject district0x/async-helpers "0.1.2-SNAPSHOT"
   :description "library of helper functions"
   :url "https://github.com/district0x/async-helpers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.taoensso/timbre "4.10.0"]
                  [district0x/error-handling "1.0.4"]
-                 [org.clojure/clojurescript "1.10.339"]]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [jamesmacaulay/cljs-promises "0.1.0"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
--- src/district/shared/async_helpers.clj
+++ src/district/shared/async_helpers.clj
@@ -23,3 +23,23 @@
     js/process
     (fn []
       ~@body)))
+
+(defmacro safe-go [& body]
+  `(cljs.core.async/go
+     (try
+       ~@body
+       (catch :default e#
+         (taoensso.timbre/error "Go block exception"
+                                (merge {:error e#
+                                        (ex-data e#)
+                                        ~(district.shared.error-handling/compiletime-info &env &form *ns*)}))
+         e#))))
+
+(defmacro <?
+  "Expects `expr` to be a channel or ReadPort which produces a value or
+  js/Error, and returns values and throws errors"
+  [expr]
+  `(let [v-or-err# (cljs.core.async/<! ~expr)]
+     (if (cljs.core/instance? js/Error v-or-err#)
+       (throw v-or-err#)
+       v-or-err#)))
